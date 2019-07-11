6dbfbe3433f5f9436b8373fee314359666e2013f
Marius Derenthal <marius.derenthal@gmail.com>
2019-07-11 11:53:39 +0200

    successful implementation of recalssification

--- MAP.py
+++ MAP.py
@@ -152,9 +152,9 @@ outDataSet = None
 '''
 # ####################################### Data ################################################################ #
 land = gdal.Open(path_data_folder + landsat)
-gt_land = land.GetGeoTransform()
-landsat_arr = land.ReadAsArray()
-
+#gt_land = land.GetGeoTransform()
+#landsat_arr = land.ReadAsArray()
+# ####################################### PROCESSING ########################################################## #
 # mosaicing
 # https://gis.stackexchange.com/questions/44003/python-equivalent-of-gdalbuildvrt#44048
 tiles_list = glob.glob(path_data_folder + modis +"*.tif")
@@ -165,9 +165,9 @@ mosaic = gdal.BuildVRT(output, tiles_list, options=vrt_options)
 # Resampling
 # https://gis.stackexchange.com/questions/234022/resampling-a-raster-from-python-without-using-gdalwarp
 inputfile = (path_data_folder + landsat)
-input = gdal.Open(inputfile)
-inputProj = input.GetProjection()
-inputTrans = input.GetGeoTransform()
+land = gdal.Open(inputfile)
+inputProj = land.GetProjection()
+inputTrans = land.GetGeoTransform()
 
 referenceProj = mosaic.GetProjection()
 referenceTrans = mosaic.GetGeoTransform()
@@ -177,36 +177,119 @@ y = mosaic.RasterYSize
 
 resampled = (path_data_folder + 'inter/resampled.tif')
 driver= gdal.GetDriverByName('GTiff')
-output = driver.Create(resampled,x,y,1,bandreference.DataType)
-output.SetGeoTransform(referenceTrans)
-output.SetProjection(referenceProj)
+resamp = driver.Create(resampled,x,y,1,bandreference.DataType)
+resamp.SetGeoTransform(referenceTrans)
+resamp.SetProjection(referenceProj)
 
-gdal.ReprojectImage(input,output,inputProj,referenceProj,gdalconst.GRA_Average) # use mean for resampling approach
+gdal.ReprojectImage(land,resamp,inputProj,referenceProj,gdalconst.GRA_Average) # use mean for resampling approach
 #del resampled
-"""
-referencefile = path_data_folder + modis + 'MODIS_EVI_h0_v0.tif'
-reference = gdal.Open(referencefile)
-referenceProj = reference.GetProjection()
-referenceTrans = reference.GetGeoTransform()
-bandreference = reference.GetRasterBand(1)
-x = reference.RasterXSize
-y = reference.RasterYSize
-
-outputfile = (path_data_folder + modis + 'resampled.tif')
-driver= gdal.GetDriverByName('GTiff')
-output = driver.Create(outputfile,x,y,1,bandreference.DataType)
-output.SetGeoTransform(referenceTrans)
-output.SetProjection(referenceProj)
-
-gdal.ReprojectImage(input,output,inputProj,referenceProj,gdalconst.GRA_Bilinear)
-
-del output
-"""
 
 
 # create mask
+modis_arr  = mosaic.ReadAsArray()
+print(modis_arr.shape)
+#modis_arr = np.delete(modis_arr, 47, axis=0) # mosaicing adds a extra band which has to be removed beforehand
+modis_arr = modis_arr[0:46,:,:]
+print("MODIS SHAPE: ",modis_arr.shape)
+#print(modis_arr.ndim)
+print("MODIS MIN: ",modis_arr.min())
+print("MODIS MAX: ",modis_arr.max())
 
-# ####################################### PROCESSING ########################################################## #
+nodata = -9999
+
+
+# apply mask
+resamp_arr = resamp.ReadAsArray()
+print("LANDSAT SHAPE: ",resamp_arr.shape)
+#print(modis_arr.ndim)
+print("LANDSAT MIN: ",resamp_arr.min())
+print("LANDSAT MAX: ",resamp_arr.max())
+
+nodatamask = modis_arr == nodata
+#print(nodatamask.shape)
+#print(nodatamask)
+
+
+
+#landsat_arr_m = resamp_arr[nodatamask] = nodata
+
+print("----------------------------------------------")
+
+# reclassify
+#print(resamp_arr)
+
+#resamp_arr[resamp_arr < 0] = 0
+resamp_arr[(resamp_arr >= 0) &(resamp_arr < 1000)] = 1
+resamp_arr[(resamp_arr >= 1000) & (resamp_arr < 2000)] = 2
+resamp_arr[(resamp_arr >= 2000) & (resamp_arr < 3000)] = 3
+resamp_arr[(resamp_arr >= 3000) & (resamp_arr < 4000)] = 4
+resamp_arr[(resamp_arr >= 4000) & (resamp_arr < 5000)] = 5
+resamp_arr[(resamp_arr >= 5000) & (resamp_arr < 6000)] = 6
+resamp_arr[(resamp_arr >= 6000) & (resamp_arr < 7000)] = 7
+resamp_arr[(resamp_arr >= 7000) & (resamp_arr < 8000)] = 8
+resamp_arr[(resamp_arr >= 8000) & (resamp_arr < 9000)] = 9
+resamp_arr[(resamp_arr >= 9000) & (resamp_arr < 10000)] = 10
+#resamp_arr[resamp_arr >= 10000 ] = 11
+
+print(resamp_arr)
+'''
+driver = gdal.GetDriverByName('GTiff')
+file = gdal.Open(path_data_folder + landsat)
+band = file.GetRasterBand(1)
+lista = band.ReadAsArray()
+resamp_arr = lista
+
+# reclassification
+for j in  range(file.RasterXSize):
+    for i in  range(file.RasterYSize):
+        if lista[i,j] >= 0 and lista[i,j] < 1000:
+            lista[i,j] = 1
+        elif lista[i,j] >= 1000 and lista[i,j] < 2000:
+            lista[i,j] = 2
+        elif lista[i, j] >= 2000 and lista[i, j] < 3000:
+            lista[i,j] = 3
+        elif lista[i,j] >= 3000 and lista[i,j] < 4000:
+            lista[i,j] = 4
+        elif lista[i,j] >= 4000 and lista[i,j] < 5000:
+            lista[i,j] = 5
+        elif lista[i,j] >= 5000 and lista[i,j] < 6000:
+            lista[i,j] = 6
+        elif lista[i,j] >= 6000 and lista[i,j] < 7000:
+            lista[i,j] = 7
+        elif lista[i,j] >= 7000 and lista[i,j] < 8000:
+            lista[i,j] = 8
+        elif lista[i,j] >= 8000 and lista[i,j] < 9000:
+            lista[i,j] = 9
+        elif lista[i,j] >= 9000 and lista[i,j] <= 10000:
+            lista[i,j] = 10
+        elif lista[i,j] >= 10001:
+            lista[i,j] = 11
+    print(j)
+
+lista[np.where(resamp_arr < 0)] = 0
+lista[np.where(resamp_arr >= 0 & resamp_arr < 1000)] = 1
+lista[np.where(resamp_arr >= 1000 & resamp_arr < 2000)] = 2
+lista[np.where(resamp_arr >= 2000 & resamp_arr < 3000)] = 3
+lista[np.where(resamp_arr >= 3000 & resamp_arr < 4000)] = 4
+lista[np.where(resamp_arr >= 4000 & resamp_arr < 5000)] = 5
+lista[np.where(resamp_arr >= 5000 & resamp_arr < 6000)] = 6
+lista[np.where(resamp_arr >= 6000 & resamp_arr < 7000)] = 7
+lista[np.where(resamp_arr >= 7000 & resamp_arr < 8000)] = 8
+lista[np.where(resamp_arr >= 8000 & resamp_arr < 9000)] = 9
+lista[np.where(resamp_arr >= 9000 & resamp_arr <= 10000)] = 10
+'''
+
+# create new file
+file2 = driver.Create(path_data_folder + 'inter/reclassified.tif', resamp.RasterXSize , resamp.RasterYSize , 1)
+file2.GetRasterBand(1).WriteArray(resamp_arr)
+
+# spatial ref system
+proj = resamp.GetProjection()
+georef = resamp.GetGeoTransform()
+file2.SetProjection(proj)
+file2.SetGeoTransform(georef)
+file2.FlushCache()
+
 
 
 # ####################################### END TIME-COUNT AND PRINT TIME STATS##################
